965a32b05e9d4ee4337c31327074ead06b7bed91
Fiorella Rey <fiorella_taz@hotmail.com>
2021-02-09 17:46:23 -0500

    editarPelicula

--- www/entradasHoy/src/java/com/miempresa/controllers/PeliculaController.java
+++ www/entradasHoy/src/java/com/miempresa/controllers/PeliculaController.java
@@ -58,11 +58,12 @@ public class PeliculaController extends HttpServlet {
     return "Short description";
   }// </editor-fold>
 
+
   private void lastest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     PeliculaDAO peliculaDAO = new PeliculaDAO();
     List<Pelicula> peliculas = new ArrayList<>();
     peliculas = peliculaDAO.seleccionarUltimos();
-    request.setAttribute("peliculas", peliculas);
+    request.getSession().setAttribute("peliculas", peliculas);
     request.getRequestDispatcher("/index.jsp").forward(request, response);
   }
 
@@ -70,7 +71,7 @@ public class PeliculaController extends HttpServlet {
     PeliculaDAO peliculaDAO = new PeliculaDAO();
     List<Pelicula> peliculas = new ArrayList<>();
     peliculas = peliculaDAO.seleccionarEstrenos();
-    request.setAttribute("peliculas", peliculas);
+    request.getSession().setAttribute("peliculas", peliculas);
     request.getRequestDispatcher("/index.jsp").forward(request, response);
   }
 
@@ -78,7 +79,7 @@ public class PeliculaController extends HttpServlet {
     PeliculaDAO peliculaDAO = new PeliculaDAO();
     List<Pelicula> peliculas = new ArrayList<>();
     peliculas = peliculaDAO.seleccionarRecomendados();
-    request.setAttribute("peliculas", peliculas);
+    request.getSession().setAttribute("peliculas", peliculas);
     request.getRequestDispatcher("/index.jsp").forward(request, response);
   }
 
@@ -86,18 +87,19 @@ public class PeliculaController extends HttpServlet {
     PeliculaDAO peliculaDAO = new PeliculaDAO();
     List<Pelicula> peliculas = new ArrayList<>();
     peliculas = peliculaDAO.seleccionarTodo();
-    request.setAttribute("peliculas", peliculas);
+    request.getSession().setAttribute("peliculas", peliculas);
     request.getRequestDispatcher("/admin/estrenos/index.jsp").forward(request, response);
   }
+ 
 
-  private void botonEditarPelicula(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private void botonEditarPelicula(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     String id = request.getParameter("idPelicula");
     PeliculaDAO peliculaDAO = new PeliculaDAO();
     Pelicula pelicula = peliculaDAO.seleccionPorId(Integer.parseInt(id.toString()));
-    request.setAttribute("pelicula", pelicula);
-    request.getRequestDispatcher("/admin/estrenos/detalle.jsp").forward(request, response);
+    request.getSession().setAttribute("pelicula", pelicula);
+    request.getRequestDispatcher("admin/estrenos/detalle.jsp").forward(request, response);
   }
-
+  
   private void editarPelicula(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     String id = request.getParameter("idPelicula");
     String nombre = request.getParameter("nombre");
@@ -107,50 +109,51 @@ public class PeliculaController extends HttpServlet {
     String pais = request.getParameter("pais");
     String subtitulos = request.getParameter("subtitulos");
     String doblada = request.getParameter("doblada");
-    String portada = request.getParameter("portada");
+    String portada = request.getParameter("portada"); 
     String descripcion = request.getParameter("descripcion");
     String genero = request.getParameter("genero");
     String estado = request.getParameter("estado");
-
+    
     Pelicula pelicula = new Pelicula();
-    pelicula.setIdPelicula(Integer.parseInt(id.toString()));
-    pelicula.setNombre(nombre);
-    pelicula.setDuracion(duracion);
-    pelicula.setFechaEstreno(fechaEstreno);
-    pelicula.setIdioma(idioma);
-    pelicula.setPais(pais);
-    pelicula.setSubtitulos(Integer.parseInt(subtitulos.toString()));
-    pelicula.setDoblada(Integer.parseInt(doblada.toString()));
-    pelicula.setPortada(portada);
-    pelicula.setDescripcion(descripcion);
-    pelicula.setGenero(genero);
-    pelicula.setEstado(Integer.parseInt(estado.toString()));
-
+      pelicula.setIdPelicula(Integer.parseInt(id.toString()));
+      pelicula.setNombre(nombre);
+      pelicula.setDuracion(duracion);
+      pelicula.setFechaEstreno(fechaEstreno);
+      pelicula.setIdioma(idioma);
+      pelicula.setPais(pais);
+      pelicula.setSubtitulos(Integer.parseInt(subtitulos.toString()));
+      pelicula.setDoblada(Integer.parseInt(doblada.toString()));
+      pelicula.setPortada(portada);
+      pelicula.setDescripcion(descripcion);
+      pelicula.setGenero(genero);
+      pelicula.setEstado(Integer.parseInt(estado.toString()));
+      
+      
     PeliculaDAO peliculaDAO = new PeliculaDAO();
-    if (peliculaDAO.actualizar(pelicula)) {
-      System.out.println("Se actualizo");
-      request.getRequestDispatcher("/PeliculaController?processing=listarPeliculas").forward(request, response);
-    } else {
-      System.out.println("error");
-      request.getRequestDispatcher("/PeliculaController?processing=listarPeliculas").forward(request, response);
-    }
-  }
-
-  private void eliminarPelicula(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    if(peliculaDAO.actualizar(pelicula)){
+        System.out.println("Se actualizo");
+        request.getRequestDispatcher("/PeliculaController?processing=listarPeliculas").forward(request, response);
+    }else{
+        System.out.println("error");
+    }    
+  }  
+  
+private void eliminarPelicula(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+      
     String id = request.getParameter("idPelicula");
-    System.out.println(id);
+      System.out.println(id);
     Pelicula pelicula = new Pelicula();
     pelicula.setIdPelicula(Integer.parseInt(id.toString()));
-    pelicula.setEstado(0);
-
+      pelicula.setEstado(0);
+      
     PeliculaDAO peliculaDAO = new PeliculaDAO();
-    if (peliculaDAO.eliminar(pelicula)) {      
-      System.out.println("Se desahiblito");
-      request.getRequestDispatcher("/PeliculaController?processing=listarPeliculas").forward(request, response);
-    } else {      
-      System.out.println("error en la deshabilitacion de la pelicula");
-      request.getRequestDispatcher("/PeliculaController?processing=listarPeliculas").forward(request, response);
-    }
+    if(peliculaDAO.eliminar(pelicula)){
+        request.getRequestDispatcher("admin/estrenos/index.jsp").forward(request, response);
+        System.out.println("Se desahiblito");
+    }else{
+        request.getRequestDispatcher("admin/estrenos/index.jsp").forward(request, response);
+        System.out.println("error en la deshabilitacion de la pelicula");
+    }   
   }
 }
+
